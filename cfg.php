a7a5c826220746baf75f6bf3924bfff284588d88
❎❎itsneutral❎❎ <77259546+neutralarmy@users.noreply.github.com>
2021-06-03 18:03:24 +0530

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,13 @@
+
+<?php
+
+//Telegram Channel:- t.me/neutralarmy
+//Youtube Channel:- youtube.com/neutralarmy
+//Discussion Group:- t.me/neutralarmydiscuss
+
+
+//Enter Your Email & Password here
+//Feel free to enter we never store your data
+
+$email = "shashanksidhi27in@gmail.com";
+$password = "shashank27";
